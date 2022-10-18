861438c232f9eb3746ef2a15a1db62b9b383f845
Edidiong Udoh(TechnicalDee) <gisthive@gmail.com>
2022-10-18 16:21:19 +0100

    fixes

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = [
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
